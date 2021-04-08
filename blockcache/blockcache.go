6b748c613aff05117ee06c3601c1f175dd01a52f
Mark Gordon <msg555@gmail.com>
2021-04-07 17:03:49 -0700

    Add blockcache container

---
+++ blockcache/blockcache.go
@@ -0,0 +1,311 @@
+package blockcache
+
+import (
+	"container/list"
+	"sync"
+)
+
+type Flushable interface {
+	FlushBlock(key interface{}, buf []byte) error
+}
+
+type cacheVal struct {
+	Buf []byte // Guarded by val.Lock
+
+	// Marks that the cache value is no longer alive. Do to the locking scheme it
+	// is possible a value is no longer good after you retrieve it from the cache
+	// so we use this flag to mark if we should refresh our value.
+	Dead bool // Guarded by val.Lock
+
+	// Locks access to this cache item. This should be locked anytime a client is
+	// accessing the data at this cache item.
+	Lock sync.Mutex
+
+	// Can be read holding cache.Lock or val.Lock, must hold both to write.
+	OldElem *list.Element
+	DirtyElem *list.Element
+
+	// Immutable upon creation
+	GroupKey interface{}
+	SubKey interface{}
+}
+
+type BlockCache struct {
+	// Current number of elements in the cache
+	Size int
+
+	// Maximum number of elements in the cache.
+	CacheSize int
+
+	// Size of block allocation for cache elements.
+	BlockSize int
+
+	// Global block cache lock. Never hold the global lock while holding a value
+	// lock. The opposite is permissable. Never block or invoke callbacks while
+	// holding this lock, it is meant to be a short-term lock.
+	lock sync.Mutex
+
+	pool sync.Pool
+
+	groupMap map[interface{}]map[interface{}]*cacheVal
+	oldList *list.List
+	dirtyList *list.List
+}
+
+func New(cacheSize, blockSize int) *BlockCache {
+	return &BlockCache{
+		Size: 0,
+		CacheSize: cacheSize,
+		BlockSize: blockSize,
+		pool: sync.Pool{
+			New: func() interface{} {
+				return make([]byte, blockSize)
+			},
+		},
+		groupMap: make(map[interface{}]map[interface{}]*cacheVal),
+		oldList: list.New(),
+		dirtyList: list.New(),
+	}
+}
+
+func (c *BlockCache) deleteValue(val *cacheVal) (bool, error) {
+	// Mark the value as dead.
+	val.Lock.Lock()
+	defer val.Lock.Unlock()
+
+	if val.Dead {
+		// Someone else already deleted/deleting this item
+		return false, nil
+	}
+
+	if val.DirtyElem != nil {
+		groupFlushable, ok := val.GroupKey.(Flushable)
+		if ok {
+			err := groupFlushable.FlushBlock(val.SubKey, val.Buf)
+			if err != nil {
+				return false, err
+			}
+		}
+	}
+
+	val.Dead = true
+	c.pool.Put(val.Buf)
+
+	return true, nil
+}
+
+func (c *BlockCache) lookup(groupKey, key interface{}, create bool) (*cacheVal, bool, error) {
+	var val *cacheVal
+	var created bool
+
+  c.lock.Lock()
+	defer c.lock.Unlock()
+
+	err := c.evict()
+	if err != nil {
+		return nil, false, err
+	}
+
+  submap, ok := c.groupMap[groupKey]
+  if create && !ok {
+    submap = make(map[interface{}]*cacheVal)
+    c.groupMap[groupKey] = submap
+    ok = true
+  }
+  if ok {
+    val, ok = submap[key]
+    if create && !ok {
+			c.Size++
+      buf := c.pool.Get().([]byte)
+			for i := 0; i < len(buf); i++ {
+				buf[i] = 0
+			}
+      val = &cacheVal{
+        Buf: buf,
+				GroupKey: groupKey,
+				SubKey: key,
+      }
+			val.OldElem = c.oldList.PushBack(val)
+			created = true
+
+			submap[key] = val
+    } else if ok {
+			c.oldList.MoveToBack(val.OldElem)
+		}
+  }
+
+	return val, created, nil
+}
+
+func (c *BlockCache) evict() error {
+	for c.CacheSize <= c.Size {
+		// Find the element we want to delete.
+		val := c.oldList.Front().Value.(*cacheVal)
+		c.lock.Unlock()
+
+		val.Lock.Lock()
+
+		if val.Dead {
+			// Someone else already handled deleting this item, skip.
+			val.Lock.Unlock()
+			c.lock.Lock()
+			continue
+		}
+
+		// Flush the element if dirty
+		if val.DirtyElem != nil {
+			groupFlushable, ok := val.GroupKey.(Flushable)
+			if ok {
+				err := groupFlushable.FlushBlock(val.SubKey, val.Buf)
+				if err != nil {
+					val.Lock.Unlock()
+					c.lock.Lock()
+					return err
+				}
+			}
+		}
+
+		// Mark the value as dead.
+		val.Dead = true
+		c.pool.Put(val.Buf)
+
+		// Regrab map lock first to ensure anyone who sees a dead value will not get
+		// that dead value again if they refresh their value.
+		c.lock.Lock()
+		val.Lock.Unlock()
+
+		// Delete from groupMap if still present (which it probably is unless
+		// someone accessed the same key again)
+		submap, ok := c.groupMap[val.GroupKey]
+		if ok {
+			curVal := submap[val.SubKey]
+			if curVal == val {
+				delete(submap, val.SubKey)
+			}
+		}
+
+		// Remove from old list and dirty list.
+		c.oldList.Remove(val.OldElem)
+		if val.DirtyElem != nil {
+			c.dirtyList.Remove(val.DirtyElem)
+		}
+
+		c.Size--
+	}
+
+	return nil
+}
+
+func (c *BlockCache) flushOne() error {
+	c.lock.Lock()
+	val := c.dirtyList.Front().Value.(*cacheVal)
+	c.lock.Unlock()
+
+	val.Lock.Lock()
+	defer val.Lock.Unlock()
+
+	if val.Dead || val.DirtyElem == nil {
+		// Element is being deleted or already flushed, skip
+		return nil
+	}
+
+	// Flush the element if dirty
+	groupFlushable := val.GroupKey.(Flushable)
+	err := groupFlushable.FlushBlock(val.SubKey, val.Buf)
+	if err != nil {
+		return err
+	}
+
+	c.lock.Lock()
+	c.dirtyList.Remove(val.DirtyElem)
+	val.DirtyElem = nil
+	c.lock.Unlock()
+
+	return nil
+}
+
+func (c *BlockCache) Access(groupKey, key interface{}, create bool, accessFunc func(buf []byte, found bool) (modified bool, err error)) error {
+	var val *cacheVal
+	var created bool
+	var err error
+	for {
+		val, created, err = c.lookup(groupKey, key, create)
+		if err != nil {
+			return err
+		}
+		if val == nil {
+			break
+		}
+
+		val.Lock.Lock()
+		if val.Dead {
+			val.Lock.Unlock()
+			continue
+		}
+
+		defer val.Lock.Unlock()
+		break
+	}
+
+	if val == nil {
+		_, err := accessFunc(nil, false)
+		return err
+	}
+
+	modified, err := accessFunc(val.Buf, created)
+	if modified {
+			// Mark element dirty
+			c.lock.Lock()
+			if val.DirtyElem == nil {
+				val.DirtyElem = c.dirtyList.PushBack(val)
+			} else {
+				c.dirtyList.MoveToBack(val.DirtyElem)
+			}
+			c.lock.Unlock()
+	}
+	return err
+}
+
+func (c *BlockCache) FlushGroup(groupKey interface{}) error {
+	var vals []*cacheVal
+
+
+	c.lock.Lock()
+	submap, ok := c.groupMap[groupKey]
+	if ok {
+		vals = make([]*cacheVal, 0, len(submap))
+		for _, val := range submap {
+			if val.DirtyElem != nil {
+				vals = append(vals, val)
+			}
+		}
+	}
+	c.lock.Unlock()
+
+	groupFlushable := groupKey.(Flushable)
+	for _, val := range vals {
+		val.Lock.Lock()
+
+		if val.Dead || val.DirtyElem == nil {
+			val.Lock.Unlock()
+			continue
+		}
+
+		// Flush the element if dirty
+		err := groupFlushable.FlushBlock(val.SubKey, val.Buf)
+		if err != nil {
+			val.Lock.Unlock()
+			return err
+		}
+
+		// Remove from dirty list
+		c.lock.Lock()
+		c.dirtyList.Remove(val.DirtyElem)
+		val.DirtyElem = nil
+		c.lock.Unlock()
+
+		val.Lock.Unlock()
+	}
+
+	return nil
+}
---
+++ blockcache/blockcache_test.go
@@ -0,0 +1,169 @@
+package blockcache
+
+import (
+	"encoding/binary"
+  "math/rand"
+	"sync"
+	"testing"
+	"time"
+)
+
+var bo = binary.LittleEndian
+
+// Ensure that multiple keys can be accessed at the same time.
+func TestParallelismManyKey(t *testing.T) {
+	numObjs := 10
+	cache := New(numObjs, 4)
+
+	wg := sync.WaitGroup{}
+	wg.Add(numObjs)
+	for i := 0; i < numObjs; i++ {
+		go func(obj int) {
+			err := cache.Access(nil, obj, true, func(buf []byte, created bool) (bool, error) {
+				if !created {
+					t.Fatal("expected element to be created")
+				}
+				wg.Done()
+				wg.Wait()
+				return false, nil
+			})
+			if err != nil {
+				t.Fatal(err)
+			}
+		}(i)
+	}
+
+	c := make(chan struct{})
+	go func() {
+		wg.Wait()
+		c <- struct{}{}
+	}()
+
+	select {
+		case <-c:
+		case <-time.After(1000000000):
+			t.Fatal("timed out waiting for routines to finish")
+	}
+}
+
+// Ensure that a single key can only be accessed once at the same time. Also
+// verifies that modifications to the buffer are made accessible to subsequent
+// accesses.
+func TestParallelismSingleKey(t *testing.T) {
+  numGoros := 100
+  cache := New(10, 4)
+
+  wg := sync.WaitGroup{}
+  wg.Add(numGoros)
+  for i := 0; i < numGoros; i++ {
+    go func() {
+      err := cache.Access(nil, nil, true, func(buf []byte, _ bool) (bool, error) {
+				bo.PutUint32(buf, bo.Uint32(buf) + 1)
+        return true, nil
+      })
+      if err != nil {
+        t.Fatal(err)
+      }
+			wg.Done()
+    }()
+  }
+
+  c := make(chan struct{})
+  go func() {
+    wg.Wait()
+    c <- struct{}{}
+  }()
+
+  select {
+    case <-c:
+    case <-time.After(1000000000):
+      t.Fatal("timed out waiting for routines to finish")
+  }
+
+	err := cache.Access(nil, nil, true, func(buf []byte, _ bool) (bool, error) {
+		bufVal := bo.Uint32(buf)
+		if bufVal != uint32(numGoros) {
+			t.Fatalf("Expected bufVal %d but got %d", numGoros, bufVal)
+		}
+		return false, nil
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+type TestMapFlusher struct {
+	Backing map[int]int
+}
+
+func (f *TestMapFlusher) FlushBlock(key interface{}, buf []byte) error {
+	f.Backing[key.(int)] = int(bo.Uint32(buf))
+	return nil
+}
+
+func TestFlushFuzz(t *testing.T) {
+  cache := New(10, 4)
+
+	group := &TestMapFlusher{
+		Backing: make(map[int]int),
+	}
+
+  rng := rand.New(rand.NewSource(555))
+	keyDomain := 20
+
+	for i := 0; i < 10000; i++ {
+		k := rng.Int() % keyDomain
+		err := cache.Access(group, k, true, func(buf []byte, created bool) (bool, error) {
+			if created {
+				bo.PutUint32(buf, uint32(group.Backing[k]) + 1)
+			} else {
+				bo.PutUint32(buf, bo.Uint32(buf) + 1)
+			}
+			return true, nil
+		})
+		if (i + 1) % 100 == 0 {
+			keyHits := 0
+			valTotal := 0
+			backedValTotal := 0
+			for j := 0; j < keyDomain; j++ {
+				err := cache.Access(group, j, false, func(buf []byte, _ bool) (bool, error) {
+					if buf == nil {
+						valTotal += group.Backing[j]
+						backedValTotal += group.Backing[j]
+					} else {
+						valTotal += int(bo.Uint32(buf))
+						keyHits++
+					}
+					return true, nil
+				})
+				if err != nil {
+					t.Fatal(err)
+				}
+			}
+			if valTotal != i + 1 {
+				t.Fatalf("did not find expected sum of values")
+			}
+			if backedValTotal >= valTotal {
+				t.Fatalf("did not expect all modifications to be in backing")
+			}
+			if keyHits != cache.Size {
+				t.Fatalf("expected number of key hits to match cache size; hits=%d cacheSize=%d", keyHits, cache.Size)
+			}
+			if cache.Size < 1 || cache.CacheSize < cache.Size {
+				t.Fatal("unexpected cache size")
+			}
+
+			valTotal = 0
+			cache.FlushGroup(group)
+			for j := 0; j < keyDomain; j++ {
+				valTotal += group.Backing[j]
+			}
+			if valTotal != i + 1 {
+				t.Fatalf("FlushGroup did not push all values to backing")
+			}
+		}
+    if err != nil {
+			t.Fatal(err)
+		}
+  }
+}
